121ddf83510c6d90ffee89915ba669941e6fa85b
Ahmad Chaiban <ahmadchaiban@gmail.com>
2020-01-05 15:08:00 +0200

    Completed Project was Added
    -- Optimization of Chicago public transportation project was added.

---
+++ Data Ingestion with Kafka and Kafka Streaming/Project - Optimizing Public Transportation/Chicago-Transportation-Optimization/consumers/models/weather.py
@@ -0,0 +1,30 @@
+"""Contains functionality related to Weather"""
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class Weather:
+    """Defines the Weather model"""
+
+    def __init__(self):
+        """Creates the weather model"""
+        self.temperature = 70.0
+        self.status = "sunny"
+
+    def process_message(self, message):
+        """Handles incoming weather data"""
+        logger.info("weather process_message is incomplete - skipping")
+        #
+        #
+        # TODO: Process incoming weather messages. Set the temperature and status.
+        #
+        #
+        f message is None:
+            print("no message received by consumer")
+        elif message.error() is not None:
+            print(f"error from consumer {message.error()}")
+        else:
+            print(f"consumed message {message.key()}: {message.value()}")
+            print(f"Temperature : {self.temperature} , Status : {self.status}")
---
+++ Data Ingestion with Kafka and Kafka Streaming/Project - Optimizing Public Transportation/Chicago-Transportation-Optimization/producers/models/producer.py
@@ -0,0 +1,101 @@
+"""Producer base-class providing common utilites and functionality"""
+import logging
+import time
+
+
+from confluent_kafka import avro
+from confluent_kafka.admin import AdminClient, NewTopic
+from confluent_kafka.avro import AvroProducer
+
+logger = logging.getLogger(__name__)
+
+BROKER_URL = "PLAINTEXT://localhost:9092"
+SCHEMA_URL = 'http://localhost:8081'
+
+class Producer:
+    """Defines and provides common functionality amongst Producers"""
+
+    # Tracks existing topics across all Producer instances
+    existing_topics = set([])
+
+    def __init__(
+        self,
+        topic_name,
+        key_schema,
+        value_schema=None,
+        num_partitions=1,
+        num_replicas=1,
+    ):
+        """Initializes a Producer object with basic settings"""
+        self.topic_name = topic_name
+        self.key_schema = key_schema
+        self.value_schema = value_schema
+        self.num_partitions = num_partitions
+        self.num_replicas = num_replicas
+
+        #
+        #
+        # TODO: Configure the broker properties below. Make sure to reference the project README
+        # and use the Host URL for Kafka and Schema Registry!
+        #
+        #
+        self.broker_properties = {
+            'listeners' : [BROKER_URL]#,SCHEMA_URL]
+        }
+
+        # If the topic does not already exist, try to create it
+        if self.topic_name not in Producer.existing_topics:
+            self.create_topic()
+            Producer.existing_topics.add(self.topic_name)
+
+        # TODO: Configure the AvroProducer
+        self.producer = AvroProducer(
+            {
+            'bootstrap.servers': BROKER_URL,
+            'schema.registry.url': SCHEMA_URL
+            },
+            default_key_schema= self.key_schema,
+            default_value_schema= self.value_schema
+         )
+
+    def create_topic(self):
+        """Creates the producer topic if it does not already exist"""
+        #
+        #
+        # TODO: Write code that creates the topic for this producer if it does not already exist on
+        # the Kafka Broker.
+        #
+        #
+        Client = AdminClient({'bootstrap.servers':BROKER_URL})
+        Client.create_topics(
+            new_topics = 
+            [
+            NewTopic(topic=self.topic_name,
+                     num_partitions=self.num_partitions,
+                     replication_factor=self.num_replicas,
+                     config={'delete.retention.ms':"2000",
+                             'cleanup.policy':'compact',
+                             'compression.type':'lz4',
+                             'file.delete.delay.ms':"2000"}
+                    )
+            ]
+        )
+        logger.info("topic creation kafka integration complete - continuing")
+
+    def time_millis(self):
+        return int(round(time.time() * 1000))
+
+    def close(self):
+        """Prepares the producer for exit by cleaning up the producer"""
+        #
+        #
+        # TODO: Write cleanup code for the Producer here
+        #
+        #
+        self.flush()
+        self.close()
+        logger.info("producer close complete - continuing")
+
+    def time_millis(self):
+        """Use this function to get the key for Kafka Events"""
+        return int(round(time.time() * 1000))
